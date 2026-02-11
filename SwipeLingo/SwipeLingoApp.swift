41b3692ab38415f6801cf2da17cd09de301f48cc
AndreyEfimovDev <andrey.efimov.dev@gmail.com>
2026-02-11 12:24:52 +0300

    Initial Commit

---
+++ SwipeLingo/SwipeLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeLingoApp.swift
+//  SwipeLingo
+//
+//  Created by Andrey Efimov on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwipeLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
